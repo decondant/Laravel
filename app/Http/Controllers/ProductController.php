4ae8ac1249e5387e685e6a57609de3dd13ab35ff
“decondant” <rikhotsohandsome.com>
2018-12-28 08:42:08 +0200

    listing projects form added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
@@ -11,4 +12,21 @@ class ProductController extends Controller
     {
         return view('products.create');
     }
+	
+	public function store(Request $request)
+	{
+        $product = $this->validate(request(), [
+          'name' => 'required',
+          'price' => 'required|numeric'
+        ]);
+        Product::create($product);
+        return back()->with('success', 'Product has been added');
+	}
+	
+	public function index()
+    {
+        $products = Product::all()->toArray();
+        return view('products.index', compact('products'));
+    }
+	
 }
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -4,13 +4,28 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Laravel 5.5 CRUD Tutorial With Example From Scratch </title>
+    <title>Laravel 5.5 CRUD Tutorial</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
   </head>
   <body>
     <div class="container">
       <h2>Create A Product</h2><br  />
-      <form method="post">
+      @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div><br />
+      @endif
+      @if (\Session::has('success'))
+      <div class="alert alert-success">
+          <p>{{ \Session::get('success') }}</p>
+      </div><br />
+      @endif
+      <form method="post" action="{{url('products')}}">
+        {{csrf_field()}}
         <div class="row">
           <div class="col-md-4"></div>
           <div class="form-group col-md-4">
